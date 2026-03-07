328418d2e2f768841a1b9d3322be251a92fa162b
orhit <rohithaudhari.19@gmail.com>
2026-03-07 05:34:00 +0000

    update

--- cieled/app/tool/page.tsx
+++
@@ -1,9 +0,0 @@
-import CIEComparator from "../components/CIEComparator";
-
-export const metadata = {
-  title: "CIE 1931 Comparator | Xuanlabs",
-};
-
-export default function ToolPage() {
-  return <CIEComparator />;
-}
